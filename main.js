37a3698892f38d789a26129603d8a37cd6ad6475
Sol Bordón <solbo@metabolismo06.InstitutoLanari.local>
2022-02-16 17:01:23 -0300

    arreglando positios relative y hago el js

---
+++ main.js
@@ -0,0 +1,31 @@
+/*
+let home = document.getElementById("homeJS");
+let wishes = document.getElementById("wishesJS");
+let complaints = document.getElementById("complaintsJS");
+
+home.addEventListener("mouseover", color);
+home.addEventListener("mouseout", color2);
+
+function color(){
+    home.style.color = "#FBFF00";
+    home.style.fontSize ="19px";
+
+    wishes.style.color = "#FF1700";
+    wishes.style.fontSize ="19px";
+
+    complaints.style.color = "#FBFF00";
+    complaints.style.fontSize ="19px";
+}
+
+function color2(){
+    home.style.color = "#FFFFFF";
+    home.style.fontSize ="18px";
+
+    wishes.style.color = "#FFFFFF";
+    wishes.style.fontSize ="18px";
+
+    complaints.style.color = "#FFFFFF";
+    complaints.style.fontSize ="18px";
+}
+
+*/
